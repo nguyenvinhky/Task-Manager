81d78b30f3f0a2c164111583a1effb57a55f386e
Vinh Ky Nguyen <info.vinhky@gmail.com>
2024-01-27 18:48:08 +0700

    update class to className

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Header() {
     return (
-        <nav class="flex items-center justify-center flex-wrap bg-teal p-6 bg-blue-700">
-            <div class="flex items-center justify-center flex-no-shrink mr-6">
-                <span class="font-semibold text-3xl tracking-tight">Task Manager</span>
+        <nav className="flex items-center justify-center flex-wrap bg-teal p-6 bg-blue-700">
+            <div className="flex items-center justify-center flex-no-shrink mr-6">
+                <span className="font-semibold text-3xl tracking-tight">Task Manager</span>
             </div>
         </nav>
     )
